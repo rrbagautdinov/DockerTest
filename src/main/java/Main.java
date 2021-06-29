c1438a817cb7b9d7f20aa61b39abd2d74166af35
bagautdinov-rr <kjwflnlkwjfne@lkajesf.asd>
2021-06-29 19:10:09 +0300

    Second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            Thread.sleep(1000);
+            System.out.println("Hello World!");
+        }
+    }
+}
